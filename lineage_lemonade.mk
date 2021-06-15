06950deb71f1e6935e7584d54dda74b690708f90
Albert Tang <tangalbert919@yahoo.com>
2021-06-15 10:19:27 -0700

    lemonade: Update audio mixer paths and build fingerprint

--- lineage_lemonade.mk
+++ lineage_lemonade.mk
@@ -34,7 +34,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus9 \
-    PRODUCT_NAME=OnePlus9 \
+    PRODUCT_NAME=OnePlus9_EEA \
     PRIVATE_BUILD_DESC="OnePlus9-user 11   release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:11/RKQ1.201105.002/2105290043:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9_EEA/OnePlus9:11/RKQ1.201105.002/2105252328:user/release-keys
