9d3c6887bb50828f55509dcff4b569ace678affe
Albert Tang <tangalbert919@yahoo.com>
2021-06-24 13:12:01 -1000

    lemonade: Use changes from LuK1337

--- device.mk
+++ device.mk
@@ -29,10 +29,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2352
+TARGET_SCREEN_HEIGHT := 2412
 TARGET_SCREEN_WIDTH := 1080
 
 # Audio
